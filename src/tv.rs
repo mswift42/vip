50d60702968f4fa433689c7a26843224aa779a42
mswift42 <martin.haesler@gmail.com>
2017-09-12 18:23:49 +0100

    Add struct Programme.

--- src/tv.rs
+++ src/tv.rs
@@ -1,14 +1,30 @@
 extern crate select;
 use select::document::Document;
-type BeebUrl = &str;
 
-pub struct
+type BeebUrl<'a> = &'a str;
+
+type TestBeebUrl = &'static str;
+
+pub struct Programme<'a> {
+    pub title: &'astr,
+    pub subtitle: &'str,
+    pub synopsis: &'str,
+    pub pid: &'str,
+    pub thumbnail: &'str,
+    pub url: &'str,
+    pub index: u16,
+}
 
 pub struct IplayerDocument {
     pub idoc: Document,
 }
 
 impl IplayerDocument {
-
+    fn new(bu: TestBeebUrl) -> IplayerDocument {
+        let idoc = Document::from(bu);
+        IplayerDocument{
+            idoc
+        }
+    }
 }
 

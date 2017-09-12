5d7d35d0258afc07b6554a0522d0b080c7423016
mswift42 <martin.haesler@gmail.com>
2017-09-12 16:03:53 +0100

    add new rust file tv

---
+++ src/tv.rs
@@ -0,0 +1,14 @@
+extern crate select;
+use select::document::Document;
+type BeebUrl = &str;
+
+pub struct
+
+pub struct IplayerDocument {
+    pub idoc: Document,
+}
+
+impl IplayerDocument {
+
+}
+

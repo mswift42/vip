4bb7197817e944b2ccf620708b1b39136d408e7b
mswift42 <martin.haesler@gmail.com>
2017-09-12 11:30:36 +0100

    Add find_title method.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate select;
+use select::document::Document;
+use select::node::Node;
+use select::predicate::{Predicate, Attr, Class, Name};
+
 fn main() {
-    println!("Hello, world!");
+    let pop = Document::from(include_str!("pop.html"));
+    for node in pop.find(Class("list-item-inner")) {
+        let title = find_title(&node);
+        println!("{}", title)
+    }
+}
+
+
+fn find_title(node: &Node) -> String {
+    node.find(Class("secondary").descendant(Class("title")))
+        .next()
+        .unwrap()
+        .text()
 }

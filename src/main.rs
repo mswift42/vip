f79f20eac44fe82258d959c0cc91897cc9c3f4d6
mswift42 <martin.haesler@gmail.com>
2017-09-29 17:41:49 +0100

    Add cpuprofiler.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 extern crate select;
+extern crate cpuprofiler;
 use select::document::Document;
 use select::node::Node;
 use select::predicate::{Attr, Class, Name, Predicate};
+use cpuprofiler::PROFILER;
+
 mod tv;
+
 fn main() {
+    PROFILER.lock().unwrap().start("mainprofile").unwrap();
     let doc = tv::IplayerDocument::new(include_str!("../testhtml/pop.html"));
-    let results = doc.programmes();
-    for i in results {
-        println!("{:?}", i);
-    }
+    PROFILER.lock().unwrap().stop().unwrap();
     let doc2 = Document::from(include_str!("../testhtml/comedy1.html"));
     for node in doc2.find(Class("page").descendant(Name("a"))) {
         println!("{:?}", node.attr("href"))

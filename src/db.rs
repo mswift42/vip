a9e548f781aa6312f7a36b92860c53896e3dd676
mswift42 <martin.haesler@gmail.com>
2017-10-08 13:08:29 +0100

    Add struct Programmedb, constructor.

--- src/db.rs
+++ src/db.rs
@@ -1,2 +1,16 @@
 extern crate serde;
 extern crate serde_json;
+extern crate time;
+
+
+use tv::Category;
+pub struct ProgrammeDB<'a> {
+    pub categoryies: Vec<Category<'a>>,
+    pub saved: time::Tm,
+}
+
+impl<'a> ProgrammeDB<'a> {
+    pub fn new<'b>(cats: Vec<Category<'a>>, saved_at: time::Tm) -> ProgrammeDB {
+        ProgrammeDB { categoryies: cats, saved: saved_at}
+    }
+}

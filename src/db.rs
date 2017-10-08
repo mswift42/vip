969ca1e618ab48f22dc81ec731c92d025a0b6566
mswift42 <martin.haesler@gmail.com>
2017-10-08 13:22:33 +0100

    Fix spelling.

--- src/db.rs
+++ src/db.rs
@@ -4,13 +4,31 @@ extern crate time;
 
 
 use tv::Category;
+#[derive(Debug)]
 pub struct ProgrammeDB<'a> {
-    pub categoryies: Vec<Category<'a>>,
+    pub categories: Vec<Category<'a>>,
     pub saved: time::Tm,
 }
 
 impl<'a> ProgrammeDB<'a> {
-    pub fn new<'b>(cats: Vec<Category<'a>>, saved_at: time::Tm) -> ProgrammeDB {
-        ProgrammeDB { categoryies: cats, saved: saved_at}
+    pub fn new<'b>(cats: Vec<Category<'a>>) -> ProgrammeDB {
+        ProgrammeDB { categories: cats, saved: time::now()}
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tv::*;
+
+    #[test]
+    fn test_programme_db() {
+        let doc = IplayerDocument::new(include_str!("../testhtml/pop.html"))
+        let progs = doc.programmes();
+        let cat := Category::new("mostpopular", &progs);
+        assert_eq!(progr[0].title, "Strike");
+        let db = ProgrammeDB::new(cat);
+        assert_eq!(db.)
     }
 }

17cb2491d8fdcac473f0201e700ffd46f4a92d27
mswift42 <martin.haesler@gmail.com>
2017-10-16 18:35:44 +0100

    Add index method + test.

--- src/db.rs
+++ src/db.rs
@@ -12,7 +12,24 @@ pub struct ProgrammeDB {
 
 impl ProgrammeDB {
     pub fn new(cats: Vec<Category>) -> ProgrammeDB {
-        ProgrammeDB { categories: cats, saved: time::now()}
+        ProgrammeDB {
+            categories: cats,
+            saved: time::now(),
+        }
+    }
+
+    pub fn save(&self) {
+
+    }
+
+    fn index(&mut self) {
+        let mut index: u32 =  0;
+        for i in &mut self.categories {
+            for j  in i.programmes.iter_mut() {
+                j.update_index(index);
+                index += 1;
+            }
+        }
     }
 }
 
@@ -27,8 +44,14 @@ mod tests {
         let doc = IplayerDocument::new(include_str!("../testhtml/pop.html"));
         let progs = doc.programmes();
         let cat = Category::new("mostpopular".to_string(), progs);
-        let db = ProgrammeDB::new(vec![cat]);
+        let mut db = ProgrammeDB::new(vec![cat]);
         assert_eq!(db.categories[0].programmes[0].title, "Strike");
         assert_eq!(db.categories[0].name, "mostpopular");
+        db.index();
+        assert_eq!(db.categories[0].programmes[1].index, 1);
+        assert_eq!(db.categories[0].programmes[2].index, 2);
+
     }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![feature(test)]
 #[macro_use]
 extern crate error_chain;
-extern crate select;
 extern crate reqwest;
+extern crate select;
 use select::document::Document;
 use select::node::Node;
 use select::predicate::{Attr, Class, Name, Predicate};
@@ -11,34 +11,34 @@ mod tv;
 mod db;
 
 fn main() {
-  //  PROFILER.lock().unwrap().start("mainprofile").unwrap();
+    //  PROFILER.lock().unwrap().start("mainprofile").unwrap();
     //let doc = tv::IplayerDocument::new(include_str!("../testhtml/pop.html"));
     //let progs = doc.programmes();
-   // PROFILER.lock().unwrap().stop().unwrap();
+    // PROFILER.lock().unwrap().stop().unwrap();
     //let doc2 = Document::from(include_str!("../testhtml/comedy1.html"));
-//    for node in doc2.find(Class("page").descendant(Name("a"))) {
-//        println!("{:?}", node.attr("href"))
-//    }
+    //    for node in doc2.find(Class("page").descendant(Name("a"))) {
+    //        println!("{:?}", node.attr("href"))
+    //    }
     // let np =
-//        doc2.find(Class("page").descendant(Name("a")))
-//            .filter_map( |n| n.attr("href"));
-//    for i in np {
-//        println!("{}", i);
-//    }
-//    let pr = tv::IplayerDocument::new(include_str!("../testhtml/pop.html"));
-//    println!("{:?}", pr.programmes());
+    //        doc2.find(Class("page").descendant(Name("a")))
+    //            .filter_map( |n| n.attr("href"));
+    //    for i in np {
+    //        println!("{}", i);
+    //    }
+    //    let pr = tv::IplayerDocument::new(include_str!("../testhtml/pop.html"));
+    //    println!("{:?}", pr.programmes());
     run();
-
-
 }
 
 fn run() -> reqwest::Result<()> {
-
     let mpophtml = reqwest::get("http://www.bbc.co.uk/iplayer/group/most-popular")?;
     let popdoc = Document::from_read(mpophtml).unwrap();
-    let idoc = tv::IplayerDocument{idoc: popdoc};
+    let idoc = tv::IplayerDocument { idoc: popdoc };
     let programmes = idoc.programmes();
-    let titles: Vec<(&str, &str)> = programmes.iter().map(|i| (&*i.title, &*i.synopsis)).collect();
+    let titles: Vec<(&str, &str)> = programmes
+        .iter()
+        .map(|i| (&*i.title, &*i.synopsis))
+        .collect();
     for i in titles {
         println!("Programme: {:?}\n", i);
     }
@@ -67,4 +67,3 @@ error_chain! {
 //
 //    Ok(())
 //}
-

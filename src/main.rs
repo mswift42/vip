c687be67cb6ae6589b6cdd28db140af71febb11d
mswift42 <martin.haesler@gmail.com>
2018-08-04 14:14:03 +0100

    Add packages url, reqwest.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+extern crate select;
+extern crate url;
+extern crate reqwest;
 
+use select::document::Document;
+
+pub trait DocumentLoader {
+    fn load(&self) -> Document;
+}
+
+pub struct IplayerDocument {
+    doc: Document
+}
+
+// type Result<IplayerDocument> = Result<IplayerDocument, >
+pub struct BeebURL<'a> {
+    url: &'a str
+}
+
+impl<'a> BeebURL<'a> {
+    fn load(&self) -> Document {
+
+    }
+}
 fn main() {
     println!("Hello, world!");
 }

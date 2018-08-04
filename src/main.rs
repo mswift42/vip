9d19fbdfbe345c9d01ef37fdb7e67340c0241397
mswift42 <martin.haesler@gmail.com>
2018-08-04 16:30:05 +0100

    add type BoxResult.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
+extern crate reqwest;
 extern crate select;
 extern crate url;
-extern crate reqwest;
+use std::error;
 
 use select::document::Document;
 
 pub trait DocumentLoader {
-    fn load(&self) -> Document;
+    fn load(&self) -> BoxResult<IplayerDocument>;
 }
-
+type BoxResult<T> = Result<T, Box<error::Error>>;
 pub struct IplayerDocument {
-    doc: Document
+    doc: Document,
 }
 
-// type Result<IplayerDocument> = Result<IplayerDocument, >
 pub struct BeebURL<'a> {
-    url: &'a str
+    url: &'a str,
 }
 
 impl<'a> BeebURL<'a> {
-    fn load(&self) -> Document {
-
+    fn load(&self) -> BoxResult<IplayerDocument> {
+        let uri = url::Url::parse(self.url)?;
+        let resp = reqwest::get(uri)?;
+       let doc = select::document::Document::from_read(resp)?;
+        Ok(IplayerDocument{doc})
     }
 }
 fn main() {

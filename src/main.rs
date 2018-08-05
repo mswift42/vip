42ebf6302503b5e21afff550d18abeb2b4e987c2
mswift42 <martin.haesler@gmail.com>
2018-08-05 12:01:19 +0100

    add type TestHTMLURL.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate reqwest;
 extern crate select;
 extern crate url;
+
 use std::error;
+use std::fs;
 
 use select::document::Document;
 
 pub trait DocumentLoader {
     fn load(&self) -> BoxResult<IplayerDocument>;
 }
+
 type BoxResult<T> = Result<T, Box<error::Error>>;
+
 pub struct IplayerDocument {
     doc: Document,
 }
@@ -17,14 +21,27 @@ pub struct BeebURL<'a> {
     url: &'a str,
 }
 
+pub struct TestHTMLURL<'a> {
+    url: &'a str
+}
+
 impl<'a> BeebURL<'a> {
     fn load(&self) -> BoxResult<IplayerDocument> {
         let uri = url::Url::parse(self.url)?;
         let resp = reqwest::get(uri)?;
-       let doc = select::document::Document::from_read(resp)?;
+        let doc = select::document::Document::from_read(resp)?;
+        Ok(IplayerDocument { doc })
+    }
+}
+
+impl<'a> TestHTMLURL<'a> {
+    fn load(&self) -> BoxResult<IplayerDocument> {
+        let html = fs::read(self.url)?;
+        let doc = Document::from_read(html)?;
         Ok(IplayerDocument{doc})
     }
 }
+
 fn main() {
     println!("Hello, world!");
 }
